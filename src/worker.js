7f021b9f4aabb65af84f535eb5e9b6af67424e1b
haruhisa-hosei <dic706@green.ocn.ne.jp>
2026-02-06 22:27:45 +0900

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,6 @@
 import monolith from "./worker.monolith.js";
 import modular from "./index.js"; // ここは後で作る（今は仮でOK）
 
-const USE_MODULAR = false; // ←最初は絶対 false
+const USE_MODULAR = true; // ←最初は絶対 false
 
 export default USE_MODULAR ? modular : monolith;
