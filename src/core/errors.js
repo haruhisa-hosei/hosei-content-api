4f386bd9dd1a31e49fa16cefc8131738ba3832ff
haruhisa-hosei <dic706@green.ocn.ne.jp>
2026-02-07 04:31:15 +0900

    Delete src directory

--- src/core/errors.js
+++
@@ -1,8 +0,0 @@
-// src/core/errors.js
-export function errorText(e) {
-  try {
-    return (e && (e.stack || e.message)) || String(e);
-  } catch {
-    return "unknown error";
-  }
-}
--- src/core/urls.js
+++
@@ -1,6 +0,0 @@
-import { nz } from "./strings.js";
-
-export function extractUrl(content) {
-  const m = nz(content).match(/https?:\/\/[\w!?\/\+\-_~=;.,*&@#$%()'[\]]+/);
-  return m ? m[0] : "";
-}
